8b8a077feb4a921e74563dd2192f4bdb1515b869
Leontiadis Konstantinos <113901432+LeonKonst@users.noreply.github.com>
2025-09-05 22:02:15 +0200

    Finish assignment of lesson Loops and arrays of TOP foundation course

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num<0){
+        return "ERROR";
+    }
+    let finalString = "";
+    for(let i = 0; i < num; i++){
+        finalString += str;
+    }
+    return finalString;
 };
+
+console.log(repeatString("hey",3))
 
 // Do not edit below this line
 module.exports = repeatString;
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...itemsToRemove) {
+    for(let itemToRemove of itemsToRemove){
+        arr = arr.filter(arrItem => arrItem !== itemToRemove);
+    }    
+    return arr;
 };
 
 // Do not edit below this line
